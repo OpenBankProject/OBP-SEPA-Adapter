76d1cca7410b0ead6c788bfb07518a912816d8fe
Guillaume KERGREIS <guillaume.kergreis@etu.univ-nantes.fr>
2020-06-08 09:14:54 +0200

    Update swagger.yaml format to OpenApi to include callbacks in webhooks

--- src/main/scala/sepa/TestWrite.scala
+++ src/main/scala/sepa/TestWrite.scala
@@ -4,7 +4,6 @@ import java.util.UUID
 
 import generated._
 import generated.`package`.defaultScope
-import generated.{AccountIdentification4Choice, ActiveCurrencyAndAmount, BranchAndFinancialInstitutionIdentification4, CLRG, CashAccount16, CreditTransferTransactionInformation11, Document, FIToFICustomerCreditTransferV02, FinancialInstitutionIdentification7, GroupHeader33, PartyIdentification32, PaymentIdentification3, PaymentTypeInformation21, Purpose2Choice, RemittanceInformation5, SLEV, ServiceLevel8Choice, SettlementInformation13}
 import javax.xml.datatype.DatatypeFactory
 import scalaxb.DataRecord
 
