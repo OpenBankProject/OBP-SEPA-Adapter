3ba95bd55283419d34bb54ce203bbe46761ea315
Guillaume Kergreis <guillaume@tesobe.com>
2020-07-02 18:40:49 +0200

    Return a transaction if account not found

--- src/main/scala/model/enums/SepaCreditTransferTransactionStatus.scala
+++ src/main/scala/model/enums/SepaCreditTransferTransactionStatus.scala
@@ -1,9 +1,14 @@
 package model.enums
 
+import model.enums
+
 object SepaCreditTransferTransactionStatus extends Enumeration {
   type SepaCreditTransferTransactionStatus = Value
 
   val PROCESSED: SepaCreditTransferTransactionStatus.Value = Value
   val UNPROCESSED: SepaCreditTransferTransactionStatus.Value = Value
   val PROCESSING_ERROR: SepaCreditTransferTransactionStatus.Value = Value
+
+  val TO_RETURN: enums.SepaCreditTransferTransactionStatus.Value = Value
+  val RETURNED: enums.SepaCreditTransferTransactionStatus.Value = Value
 }
