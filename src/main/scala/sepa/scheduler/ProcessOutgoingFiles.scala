1643f1bf1d8b988440df187360a2c690a741f3ca
Guillaume Kergreis <guillaume@tesobe.com>
2020-08-11 17:27:20 +0200

    Add all message types to ProcessOutgoingFiles

--- src/main/scala/sepa/scheduler/ProcessOutgoingFiles.scala
+++ src/main/scala/sepa/scheduler/ProcessOutgoingFiles.scala
@@ -7,8 +7,7 @@ import java.util.UUID
 import model.enums.SepaMessageType.{SepaMessageType, _}
 import model.enums._
 import model.{SepaCreditTransferTransaction, SepaFile, SepaMessage}
-import sepa._
-import sepa.sct.message.{CreditTransferMessage, PaymentRecallMessage, PaymentRecallNegativeAnswerMessage, PaymentReturnMessage, SctMessage}
+import sepa.sct.message._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
@@ -26,21 +25,11 @@ object ProcessOutgoingFiles extends App {
       updatedMessage <- Future(message.copy(status = SepaMessageStatus.PROCESSING_IN_PROGRESS, settlementDate = Some(settlementDate)))
       _ <- updatedMessage.update()
       sctMessage: SctMessage <- SepaCreditTransferTransaction.getBySepaMessageId(updatedMessage.id).map(transactions =>
-        message.messageType match {
-          case B2B_CREDIT_TRANSFER => CreditTransferMessage(updatedMessage, transactions)
-          case B2B_PAYMENT_RETURN => PaymentReturnMessage(updatedMessage, transactions)
-          case B2B_PAYMENT_RECALL => PaymentRecallMessage(updatedMessage, transactions)
-          case B2B_PAYMENT_RECALL_NEGATIVE_ANSWER => PaymentRecallNegativeAnswerMessage(updatedMessage, transactions)
-        })
+        getSctMessageByMessageType(updatedMessage, transactions))
       _ <- Future(XML.save(outgoingFile.path.toString, sctMessage.toXML.head, "UTF-8", xmlDecl = true, null))
       _ <- outgoingFile.insert()
       _ <- Future.sequence(sctMessage.creditTransferTransactions.map(transaction =>
-        (message.messageType match {
-          case B2B_CREDIT_TRANSFER => transaction._1.copy(status = SepaCreditTransferTransactionStatus.TRANSFERED, settlementDate = Some(settlementDate))
-          case B2B_PAYMENT_RETURN => transaction._1.copy(status = SepaCreditTransferTransactionStatus.RETURNED)
-          case B2B_PAYMENT_RECALL => transaction._1.copy(status = SepaCreditTransferTransactionStatus.RECALLED)
-          case B2B_PAYMENT_RECALL_NEGATIVE_ANSWER => transaction._1.copy(status = SepaCreditTransferTransactionStatus.RECALL_REJECTED)
-        }).update()))
+        getProcessedTransactionStatusByMessageType(sctMessage.message, transaction._1, Some(settlementDate)).update()))
       _ <- sctMessage.message.copy(sepaFileId = Some(outgoingFile.id), status = SepaMessageStatus.PROCESSED).update()
       _ <- outgoingFile.copy(status = SepaFileStatus.PROCESSED, processedDate = Some(LocalDateTime.now())).update()
       _ <- Future(println(s"${sctMessage.creditTransferTransactions.length} transactions accounted in file ${outgoingFile.name}"))
@@ -61,4 +50,37 @@ object ProcessOutgoingFiles extends App {
       processedDate = None
     )
   }
+
+  def getSctMessageByMessageType(sepaMessage: SepaMessage, sepaCreditTransferTransactions: Seq[(SepaCreditTransferTransaction, String)]): SctMessage = {
+    sepaMessage.messageType match {
+      case B2B_CREDIT_TRANSFER => CreditTransferMessage(sepaMessage, sepaCreditTransferTransactions)
+      case B2B_PAYMENT_RETURN => PaymentReturnMessage(sepaMessage, sepaCreditTransferTransactions)
+      case B2B_PAYMENT_RECALL => PaymentRecallMessage(sepaMessage, sepaCreditTransferTransactions)
+      case B2B_PAYMENT_RECALL_NEGATIVE_ANSWER => PaymentRecallNegativeAnswerMessage(sepaMessage, sepaCreditTransferTransactions)
+      case B2B_INQUIRY_CLAIM_NON_RECEIP => InquiryClaimNonReceiptMessage(sepaMessage, sepaCreditTransferTransactions)
+      case B2B_INQUIRY_CLAIM_VALUE_DATE_CORRECTION => InquiryClaimValueDateCorrectionMessage(sepaMessage, sepaCreditTransferTransactions)
+      case B2B_INQUIRY_CLAIM_NON_RECEIP_POSITIVE_RESPONSE => InquiryClaimNonReceiptPositiveAnswerMessage(sepaMessage, sepaCreditTransferTransactions)
+      case B2B_INQUIRY_CLAIM_NON_RECEIP_NEGATIVE_RESPONSE => InquiryClaimNonReceiptNegativeAnswerMessage(sepaMessage, sepaCreditTransferTransactions)
+      case B2B_INQUIRY_CLAIM_VALUE_DATE_CORRECTION_POSITIVE_RESPONSE => InquiryClaimValueDateCorrectionPositiveAnswerMessage(sepaMessage, sepaCreditTransferTransactions)
+      case B2B_INQUIRY_CLAIM_VALUE_DATE_CORRECTION_NEGATIVE_RESPONSE => InquiryClaimValueDateCorrectionNegativeAnswerMessage(sepaMessage, sepaCreditTransferTransactions)
+      case B2B_REQUEST_STATUS_UPDATE => RequestStatusUpdateMessage(sepaMessage, sepaCreditTransferTransactions)
+    }
+  }
+
+  def getProcessedTransactionStatusByMessageType(sepaMessage: SepaMessage, sepaCreditTransferTransaction: SepaCreditTransferTransaction, settlementDate: Option[LocalDate]): SepaCreditTransferTransaction = {
+    sepaMessage.messageType match {
+      case B2B_CREDIT_TRANSFER => sepaCreditTransferTransaction.copy(status = SepaCreditTransferTransactionStatus.TRANSFERED, settlementDate = settlementDate)
+      case B2B_PAYMENT_RETURN => sepaCreditTransferTransaction.copy(status = SepaCreditTransferTransactionStatus.RETURNED)
+      case B2B_PAYMENT_RECALL => sepaCreditTransferTransaction.copy(status = SepaCreditTransferTransactionStatus.RECALLED)
+      case B2B_PAYMENT_RECALL_NEGATIVE_ANSWER => sepaCreditTransferTransaction.copy(status = SepaCreditTransferTransactionStatus.RECALL_REJECTED)
+      case B2B_INQUIRY_CLAIM_NON_RECEIP => sepaCreditTransferTransaction.copy(status = SepaCreditTransferTransactionStatus.CLAIMED_NON_RECEIPT)
+      case B2B_INQUIRY_CLAIM_VALUE_DATE_CORRECTION => sepaCreditTransferTransaction.copy(status = SepaCreditTransferTransactionStatus.CLAIMED_VALUE_DATE_CORRECTION)
+      case B2B_INQUIRY_CLAIM_NON_RECEIP_POSITIVE_RESPONSE => sepaCreditTransferTransaction.copy(status = SepaCreditTransferTransactionStatus.CLAIM_NON_RECEIPT_ACCEPTED)
+      case B2B_INQUIRY_CLAIM_NON_RECEIP_NEGATIVE_RESPONSE => sepaCreditTransferTransaction.copy(status = SepaCreditTransferTransactionStatus.CLAIM_NON_RECEIPT_REJECTED)
+      case B2B_INQUIRY_CLAIM_VALUE_DATE_CORRECTION_POSITIVE_RESPONSE => sepaCreditTransferTransaction.copy(status = SepaCreditTransferTransactionStatus.CLAIM_VALUE_DATE_CORRECTION_ACCEPTED)
+      case B2B_INQUIRY_CLAIM_VALUE_DATE_CORRECTION_NEGATIVE_RESPONSE => sepaCreditTransferTransaction.copy(status = SepaCreditTransferTransactionStatus.CLAIM_VALUE_DATE_CORRECTION_REJECTED)
+      case B2B_REQUEST_STATUS_UPDATE => sepaCreditTransferTransaction.copy(status = SepaCreditTransferTransactionStatus.REQUESTED_STATUS_UPDATE)
+    }
+  }
+
 }
